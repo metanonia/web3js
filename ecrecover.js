4b461544da6e9022ddad91f456095cc112c917a2
Metanonia <metanonia@outlook.kr>
2023-04-07 19:11:23 +0900

    first commit

---
+++ ecrecover.js
@@ -0,0 +1,19 @@
+const ethUtil = require("ethereumjs-util");
+
+function ecRecover(message, signature) {
+  const res = ethUtil.fromRpcSig("0x" + signature);
+
+  const msg = ethUtil.keccak256(new Buffer.from(message));
+  const prefix = new Buffer.from("\x19Ethereum Signed Message:\n");
+  const prefixedMsg = ethUtil.keccak256(
+    Buffer.concat([prefix, new Buffer.from(String(msg.length)), msg])
+  );
+  const pub = ethUtil.ecrecover(prefixedMsg, res.v, res.r, res.s);
+  // ethUtil.int;
+
+  //const pub = ethUtil.ecrecover(msgBuffer, res.v, res.r, res.s);
+  const addrBuf = ethUtil.pubToAddress(pub);
+  const addr = ethUtil.bufferToHex(addrBuf);
+
+  return addr;
+}
